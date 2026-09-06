82547557b2dd5518947fea8f15c48de3450f7a76
Muhammad Daffa Ma'arif <daffamaarif.dev@gmail.com>
2026-09-06 19:25:17 +0700

    style(branding): update high-res PWA icons, favicon, and visual styles

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  base: '/jarvis/',
+  base: '/',
   plugins: [svelte()],
   server: {
     port: 3000
